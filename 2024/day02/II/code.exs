4148e4666116dd9f030085025a31d6369575a9ff
Dulaj Disanayaka <vidura.me@gmail.com>
2024-12-03 23:03:35 +0100

    day02-II (bruteforce solution)

--- 2024/day02/II/code.exs
+++ 2024/day02/II/code.exs
@@ -1,64 +1,56 @@
+# THIS IS A DISGRACE
+# Bruteforce solution
+# I just wanted to finish this and move on to the next one
+
 defmodule M do
-  def map_state(array) do
-    Enum.chunk_every(array, 2, 1, :discard)
-    |> Enum.map(fn [a, b] ->
-      val =
-        if !(abs(a - b) in 1..3),
-          do: "invalid",
-          else:
-            (cond do
-               a > b -> "decreasing"
-               a < b -> "increasing"
-               a == b -> "invalid"
-             end)
+  def same_direction(array) do
+    Enum.all?(array, fn [x, _] -> x in [:increasing, :skipped] end) ||
+      Enum.all?(array, fn [x, _] -> x in [:decreasing, :skipped] end)
+  end
 
-      [[a, b], val]
+  def all_safe(array) do
+    same_direction(array) &&
+      Enum.all?(array, fn [_, [a, b, _]] -> a == nil || abs(a - b) in 1..3 end)
+  end
+
+  def one_wrong_direction(array) do
+    array_len = Enum.count(array)
+
+    array
+    |> Enum.reject(fn [x, _] -> x == :skipped end)
+    |> Enum.frequencies_by(fn [x, _] -> x end)
+    |> Enum.any?(fn {_, count} -> count == array_len - 2 end)
+
+    # Subtract 2 because we have also removed the head  
+    true
+  end
+
+  def drop_mutations(array_with_index, drop) do
+    for i <- drop do
+      array_with_index
+      |> Enum.reject(fn {_, index} -> index == i end)
+    end
+  end
+
+  def stamp_states(arrays) do
+    arrays
+    |> Enum.map(fn array ->
+      Enum.chunk_every([nil] ++ array ++ [nil], 3, 1, :discard)
     end)
-  end
+    |> Enum.map(fn array ->
+      array
+      |> Enum.map(fn [a, b, c] ->
+        direction =
+          cond do
+            a == nil -> :skipped
+            a > b -> :decreasing
+            a < b -> :increasing
+            a == b -> :plateau
+          end
 
-  def is_all_safe(array) do
-    states = Enum.map(array, fn [_, val] -> val end)
-
-    Enum.all?(states, fn x -> x == "increasing" end) ||
-      Enum.all?(states, fn x -> x == "decreasing" end)
-  end
-
-  def is_safe_if_one_removed(array) do
-    states = Enum.map(array, fn [_, state] -> state end)
-
-    count = Enum.count(states)
-
-    increasing_count = Enum.count(states, fn x -> x == "increasing" end)
-    decreasing_count = Enum.count(states, fn x -> x == "decreasing" end)
-
-    filter =
-      cond do
-        count - increasing_count == 1 -> "increasing"
-        count - decreasing_count == 1 -> "decreasing"
-        true -> "rejected"
-      end
-
-    if filter == "rejected",
-      do: false,
-      else:
-        (
-          flat = array |> Enum.map(fn [[a, _], state] -> [a, state] end)
-
-          [[_, last], _] = Enum.at(array, -1)
-          flat = flat ++ [[last, filter]]
-
-          filtered =
-            flat
-            |> Enum.filter(fn [_, state] -> state == filter end)
-            |> Enum.map(fn [x, _] -> x end)
-
-          is_all_safe(map_state(filtered))
-        )
-  end
-
-  def is_safe(array) do
-    is_all_safe(array) ||
-      is_safe_if_one_removed(array)
+        [direction, [a, b, c]]
+      end)
+    end)
   end
 end
 
@@ -72,15 +64,69 @@ content =
     |> Enum.map(&String.to_integer/1)
   end)
 
-grouped =
-  content
-  |> Enum.map(fn x ->
-    x
-    |> M.map_state()
+state = M.stamp_states(content)
+
+same_path_safe =
+  state
+  |> Enum.filter(&M.all_safe(&1))
+
+potentially_fixable =
+  state
+  |> Enum.reject(&M.same_direction(&1))
+  |> Enum.filter(&M.one_wrong_direction(&1))
+  |> Enum.map(fn array ->
+    array
+    |> Enum.map(fn [_, [a, b, c]] -> [a, b, c] end)
+    |> Enum.with_index()
   end)
 
-count =
-  grouped
-  |> Enum.count(fn x -> M.is_safe(x) end)
+fixable_indexes =
+  potentially_fixable
+  |> Enum.map(fn array ->
+    array
+    |> Enum.filter(fn {[a, b, c], _} ->
+      cond do
+        a == nil -> if abs(b - c) in 1..3, do: false, else: true
+        c == nil -> if abs(a - b) in 1..3, do: false, else: true
+        a < b && b < c -> false
+        a > b && b > c -> false
+        true -> true
+      end
+    end)
+    |> Enum.map(fn {_, index} -> index end)
+  end)
 
-IO.inspect(count)
+fix_mutations =
+  Enum.zip(potentially_fixable, fixable_indexes)
+  |> Enum.map(fn {array, drop} -> M.drop_mutations(array, drop) end)
+  |> Enum.map(fn mutations ->
+    Enum.map(mutations, fn mutation ->
+      Enum.map(mutation, fn {[_, val, _], _} -> val end)
+    end)
+  end)
+
+edge_case =
+  state
+  |> Enum.reject(&M.all_safe(&1))
+  |> Enum.filter(&M.same_direction(&1))
+  |> Enum.map(fn array ->
+    array
+    |> Enum.map(fn [_, [a, b, c]] -> [a, b, c] end)
+    |> Enum.with_index()
+  end)
+  |> Enum.map(fn array -> M.drop_mutations(array, [0, Enum.count(array) - 1]) end)
+  |> Enum.map(fn mutations ->
+    Enum.map(mutations, fn mutation ->
+      Enum.map(mutation, fn {[_, val, _], _} -> val end)
+    end)
+  end)
+
+fixed_safe =
+  (fix_mutations ++ edge_case)
+  |> Enum.filter(fn mutations ->
+    M.stamp_states(mutations)
+    |> Enum.any?(&M.all_safe(&1))
+  end)
+
+result = Enum.count(same_path_safe) + Enum.count(fixed_safe)
+IO.inspect(result)
